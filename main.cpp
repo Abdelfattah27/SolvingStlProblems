7c56992cccb875931fb455a951f731219700271b
Abdelfattah Hamdi <76149342+Abdelfattah27@users.noreply.github.com>
2022-03-09 14:29:03 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,45 @@
+/// problem A
+/// http://codeforces.com/group/nMgBPsURd2/contest/339715/problem/A
+
+
+#include<bits/stdc++.h>
+using namespace std ;
+stack <int>st ;  // the nearest index of "(" ;
+
+string s ;
+int main()
+{
+    int t ;
+    cin>>t ;
+    while(t--)
+    {
+        int frq[200] = {};
+
+        cin>>s ;
+        for(int i = 0 ; i < s.size() ; i++)
+        {
+            char c = s[i] ;
+            if(c == '{')
+              st.push( 1) ;
+            else if(c ==',')
+            {
+                st.top() ++ ;
+            }
+            else if(c == '}')
+            {
+                frq[st.size()] = max( frq[st.size()] , st.top()) ;
+                st.pop() ;
+            }
+
+        }
+       for(int i = 1 ; i < 200 ; i++)
+       {
+           if(frq[i])
+            cout<<'['<<frq[i]<<']' ;
+           else
+            break ;
+       }
+       cout<<endl;
+    }
+
+}
